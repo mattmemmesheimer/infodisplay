d0c5b368895f05a59df9815b8b03f231b1d6f460
matt.memmseheimer <m.memmesheimer@gmail.com>
2015-07-10 17:51:39 +0100

    Added a little flexibility to the URLs in open weather map service.

--- InfoDisplay/InfoDisplay.WeatherService/Services/OpenWeatherMap/OpenWeatherMapService.cs
+++ InfoDisplay/InfoDisplay.WeatherService/Services/OpenWeatherMap/OpenWeatherMapService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using InfoDisplay.Common.Network;
 using InfoDisplay.WeatherService.Models;
 using InfoDisplay.WeatherService.Services.OpenWeatherMap.Models;
@@ -9,6 +10,25 @@ namespace InfoDisplay.WeatherService.Services.OpenWeatherMap
 
     public class OpenWeatherMapService : IWeatherService
     {
+        private const string ServiceBaseUrl = "http://api.openweathermap.org/data/2.5";
+        private const string ServiceApiKey = "0cc104f74d8d39c55e7c3806fe844861";
+        private const string ServiceUnits = "imperial";
+
+        private enum WeatherAction
+        {
+            Current,
+            Forecast
+        }
+
+        private string ServiceUrl
+        {
+            get
+            {
+                return string.Format("{0}?APPID={1}&units={2}", ServiceBaseUrl, ServiceApiKey,
+                    ServiceUnits);
+            }
+        }
+
         public OpenWeatherMapService(INetworkService networkService)
         {
             _networkService = networkService;
@@ -16,12 +36,25 @@ namespace InfoDisplay.WeatherService.Services.OpenWeatherMap
 
         public async Task<IWeatherResults> GetWeatherAsync()
         {
-            string url =
-                "http://api.openweathermap.org/data/2.5/weather?zip=78759,us&APPID=0cc104f74d8d39c55e7c3806fe844861&units=imperial";
-
+            var url = GetUrl(WeatherAction.Current, "65856");
             var json = await _networkService.GetStringAsync(url);
-
             return JsonConvert.DeserializeObject<WeatherResults>(json);
+        }
+
+        private string GetUrl(WeatherAction action, string zip)
+        {
+            string actionStr = string.Empty;
+            switch (action)
+            {
+                case WeatherAction.Current:
+                    actionStr = "weather";
+                    break;
+                case WeatherAction.Forecast:
+                    actionStr = "forecast";
+                    break;
+            }
+            return string.Format("{0}/{1}?APPID={2}&units={3}&zip={4}", ServiceBaseUrl, actionStr,
+                ServiceApiKey, ServiceUnits, zip);
         }
 
         private readonly INetworkService _networkService;
