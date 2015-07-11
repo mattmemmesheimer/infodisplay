828ddb92d964636ef4903eec27bca88251ff3d2a
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-10 23:33:07 -0500

    Prepared for adding weather vector images.

---
+++ InfoDisplay/InfoDisplay.Gui/UserControls/VectorImage.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace InfoDisplay.Gui.UserControls
+{
+    public class VectorImage : ContentControl
+    {
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register("Color", typeof (Brush), typeof (VectorImage), null);
+
+        public Brush Color
+        {
+            get { return (Brush) GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+    }
+}
