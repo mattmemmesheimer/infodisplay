092990da79cd5251a4f542fa21a90705288f4c2d
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-11 14:08:53 -0500

    Forgot to actually add the converter...

---
+++ InfoDisplay/InfoDisplay.Gui/Converters/WeatherConditionIconConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using InfoDisplay.WeatherService.Models;
+
+namespace InfoDisplay.Gui.Converters
+{
+
+    public class WeatherConditionIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var condition = (WeatherCondition) value;
+            var resources = Application.Current.Resources;
+            string key = string.Empty;
+            switch (condition)
+            {
+                case WeatherCondition.DayClear:
+                    key = "WeatherDayClearIcon";
+                    break;
+                case WeatherCondition.NightClear:
+                    key = "WeatherNightClearIcon";
+                    break;
+                case WeatherCondition.DayCloudy:
+                    key = "WeatherDayCloudyIcon";
+                    break;
+            }
+            if (key == string.Empty)
+            {
+                return Binding.DoNothing;
+            }
+            return (ControlTemplate) resources[key];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
