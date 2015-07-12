af3390d12d5ef2ca4af04e7739406c7228423ef0
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-12 17:17:03 -0500

    Removed code behind from the weather view.

--- InfoDisplay/InfoDisplay.Gui/UserControls/Weather.xaml.cs
+++ InfoDisplay/InfoDisplay.Gui/UserControls/Weather.xaml.cs
@@ -1,32 +1,13 @@
-﻿using System;
-using System.Windows;
-using InfoDisplay.Gui.ViewModels;
-
-namespace InfoDisplay.Gui.UserControls
+﻿namespace InfoDisplay.Gui.UserControls
 {
     /// <summary>
     /// Interaction logic for Weather.xaml
     /// </summary>
     public partial class Weather
     {
-
         public Weather()
         {
             InitializeComponent();
-            Loaded += OnLoaded;
         }
-
-        private async void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
-        {
-            _viewModel = DataContext as IWeatherViewModel;
-            if (_viewModel == null)
-            {
-                throw new Exception("Missing IWeatherViewModel");
-            }
-            await _viewModel.GetWeatherAsync();
-        }
-
-
-        private IWeatherViewModel _viewModel;
     }
 }
