68329712483467688656e812d3f8ea99f809233e
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-12 17:35:55 -0500

    Commenting.

--- InfoDisplay/InfoDisplay.Gui/UserControls/VectorImage.cs
+++ InfoDisplay/InfoDisplay.Gui/UserControls/VectorImage.cs
@@ -4,11 +4,20 @@ using System.Windows.Media;
 
 namespace InfoDisplay.Gui.UserControls
 {
+    /// <summary>
+    /// <see cref="ContentControl"/> with a <see cref="Brush"/> property used to render SVG data.
+    /// </summary>
     public class VectorImage : ContentControl
     {
+        /// <summary>
+        /// Color dependency property.
+        /// </summary>
         public static readonly DependencyProperty ColorProperty =
             DependencyProperty.Register("Color", typeof (Brush), typeof (VectorImage), null);
 
+        /// <summary>
+        /// Color of the vector image.
+        /// </summary>
         public Brush Color
         {
             get { return (Brush) GetValue(ColorProperty); }
