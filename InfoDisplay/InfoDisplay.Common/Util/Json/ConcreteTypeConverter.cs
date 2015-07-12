ad21963b5ff677f15b49dad35e07dc302a9a389e
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-12 02:15:44 -0500

    Commenting.

--- InfoDisplay/InfoDisplay.Common/Util/Json/ConcreteTypeConverter.cs
+++ InfoDisplay/InfoDisplay.Common/Util/Json/ConcreteTypeConverter.cs
@@ -3,18 +3,26 @@ using Newtonsoft.Json;
 
 namespace InfoDisplay.Common.Util.Json
 {
+    /// <summary>
+    /// Converts a JSON object to the specified concrete type.
+    /// </summary>
+    /// <remarks>Intended to handle de-serialization of interfaces.</remarks>
+    /// <typeparam name="TConcrete">Concrete type to convert to.</typeparam>
     public class ConcreteTypeConverter<TConcrete> : JsonConverter
     {
+        /// <see cref="JsonConverter.WriteJson"/>
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             throw new NotImplementedException();
         }
 
+        /// <see cref="JsonConverter.ReadJson"/>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             return serializer.Deserialize<TConcrete>(reader);
         }
 
+        /// <see cref="JsonConverter.CanConvert"/>
         public override bool CanConvert(Type objectType)
         {
             return true;
--- InfoDisplay/InfoDisplay.Common/Util/Json/UnixDateTimeConverter.cs
+++ InfoDisplay/InfoDisplay.Common/Util/Json/UnixDateTimeConverter.cs
@@ -3,19 +3,25 @@ using Newtonsoft.Json;
 
 namespace InfoDisplay.Common.Util.Json
 {
+    /// <summary>
+    /// Converts between an integer Unix timestamp and <see cref="DateTime" />.
+    /// </summary>
     public class UnixDateTimeConverter : JsonConverter
     {
+        /// <see cref="JsonConverter.WriteJson"/>
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             throw new NotImplementedException();
         }
 
+        /// <see cref="JsonConverter.ReadJson"/>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             var timestamp = serializer.Deserialize<int>(reader);
             return DateTimeExtensions.FromUnixTimestamp(timestamp);
         }
 
+        /// <see cref="JsonConverter.CanConvert"/>
         public override bool CanConvert(Type objectType)
         {
             return true;
