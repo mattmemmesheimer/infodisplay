07ba21567468bf443a57834cd9e9af1637aa323f
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-12 13:01:39 -0500

    Commenting.

--- InfoDisplay/InfoDisplay.Gui/ViewModels/IWeatherViewModel.cs
+++ InfoDisplay/InfoDisplay.Gui/ViewModels/IWeatherViewModel.cs
@@ -4,17 +4,35 @@ using InfoDisplay.WeatherService.Models;
 
 namespace InfoDisplay.Gui.ViewModels
 {
-
+    /// <summary>
+    /// Defines an interface for the WetherViewModel.
+    /// </summary>
     public interface IWeatherViewModel
     {
+        /// <summary>
+        /// Current weather information.
+        /// </summary>
         IWeatherResults Current { get; set; }
 
+        /// <summary>
+        /// Forecasted weather information.
+        /// </summary>
         IForecastResults Forecast { get; set; }
 
+        /// <summary>
+        /// Whether or not weather information is loading.
+        /// </summary>
         bool Loading { get; set; }
 
+        /// <summary>
+        /// Command to refresh weather information.
+        /// </summary>
         ICommand RefreshCommand { get; }
 
+        /// <summary>
+        /// Asynchronously retrieves weather information.
+        /// </summary>
+        /// <returns>Void task.</returns>
         Task GetWeatherAsync();
     }
 
--- InfoDisplay/InfoDisplay.Gui/ViewModels/WeatherViewModel.cs
+++ InfoDisplay/InfoDisplay.Gui/ViewModels/WeatherViewModel.cs
@@ -7,35 +7,50 @@ using Microsoft.Practices.Prism.Mvvm;
 
 namespace InfoDisplay.Gui.ViewModels
 {
+    /// <summary>
+    /// Concrete implementation of <see cref="IWeatherViewModel"/>.
+    /// </summary>
     public class WeatherViewModel : BindableBase, IWeatherViewModel
     {
+        #region Properties
+
+        /// <see cref="IWeatherViewModel.Current" />
         public IWeatherResults Current
         {
             get { return _current; }
             set { SetProperty(ref _current, value); }
         }
 
+        /// <see cref="IWeatherViewModel.Forecast" />
         public IForecastResults Forecast
         {
             get { return _forecast; }
             set { SetProperty(ref _forecast, value); }
         }
 
+        /// <see cref="IWeatherViewModel.Loading" />
         public bool Loading
         {
             get { return _loading; }
             set { SetProperty(ref _loading, value); }
         }
 
+        /// <see cref="IWeatherViewModel.RefreshCommand" />
         public ICommand RefreshCommand { get; private set; }
 
+        #endregion
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="weatherService">Weather service to use.</param>
         public WeatherViewModel(IWeatherService weatherService)
         {
             _weatherService = weatherService;
-
             RefreshCommand = new AwaitableDelegateCommand(GetWeatherAsync);
         }
 
+        /// <see cref="IWeatherViewModel.GetWeatherAsync" />
         public async Task GetWeatherAsync()
         {
             Loading = true;
@@ -44,10 +59,14 @@ namespace InfoDisplay.Gui.ViewModels
             Loading = false;
         }
 
+        #region Fields
+
         private double _temperature;
         private IWeatherService _weatherService;
         private IWeatherResults _current;
         private IForecastResults _forecast;
         private bool _loading;
+
+        #endregion
     }
 }
