db20ebd112b802375237118a2f2b4ed5fe8d4550
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-12 02:33:16 -0500

    Commenting.

--- InfoDisplay/InfoDisplay.WeatherService/IWeatherService.cs
+++ InfoDisplay/InfoDisplay.WeatherService/IWeatherService.cs
@@ -3,11 +3,21 @@ using InfoDisplay.WeatherService.Models;
 
 namespace InfoDisplay.WeatherService
 {
-
+    /// <summary>
+    /// Defines a weather service interface.
+    /// </summary>
     public interface IWeatherService
     {
+        /// <summary>
+        /// Asynchronously retrieves the current weather information.
+        /// </summary>
+        /// <returns>The current weather information.</returns>
         Task<IWeatherResults> GetWeatherAsync();
 
+        /// <summary>
+        /// Asynchronously retrieves weather forecast information.
+        /// </summary>
+        /// <returns>Weather forecast information.</returns>
         Task<IForecastResults> GetForecastAsync();
     }
 
--- InfoDisplay/InfoDisplay.WeatherService/Services/OpenWeatherMap/OpenWeatherMapService.cs
+++ InfoDisplay/InfoDisplay.WeatherService/Services/OpenWeatherMap/OpenWeatherMapService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using InfoDisplay.Common.Network;
 using InfoDisplay.WeatherService.Models;
 using InfoDisplay.WeatherService.Services.OpenWeatherMap.Models;
@@ -7,12 +6,23 @@ using Newtonsoft.Json;
 
 namespace InfoDisplay.WeatherService.Services.OpenWeatherMap
 {
-
+    /// <summary>
+    /// Concrete implementation of <see cref="IWeatherService"/> for the Open Weather Map service.
+    /// </summary>
+    /// <remarks>
+    /// See http://openweathermap.org/api for the API documentation.
+    /// </remarks>
     public class OpenWeatherMapService : IWeatherService
     {
+        #region Constants 
+
         private const string ServiceBaseUrl = "http://api.openweathermap.org/data/2.5";
         private const string ServiceApiKey = "0cc104f74d8d39c55e7c3806fe844861";
         private const string ServiceUnits = "imperial";
+
+        #endregion
+
+        #region Enums
 
         private enum WeatherAction
         {
@@ -20,11 +30,18 @@ namespace InfoDisplay.WeatherService.Services.OpenWeatherMap
             Forecast
         }
 
+        #endregion
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="networkService">Network service to use for data retrieval.</param>
         public OpenWeatherMapService(INetworkService networkService)
         {
             _networkService = networkService;
         }
 
+        /// <see cref="IWeatherService.GetWeatherAsync"/>
         public async Task<IWeatherResults> GetWeatherAsync()
         {
             var url = GetUrl(WeatherAction.Current, "78759");
@@ -32,6 +49,7 @@ namespace InfoDisplay.WeatherService.Services.OpenWeatherMap
             return JsonConvert.DeserializeObject<WeatherResults>(json);
         }
 
+        /// <see cref="IWeatherService.GetForecastAsync"/>
         public async Task<IForecastResults> GetForecastAsync()
         {
             var url = GetUrl(WeatherAction.Forecast, "78759");
@@ -56,7 +74,11 @@ namespace InfoDisplay.WeatherService.Services.OpenWeatherMap
                 ServiceApiKey, ServiceUnits, zip);
         }
 
+        #region Fields
+
         private readonly INetworkService _networkService;
+
+        #endregion
     }
 
 }
