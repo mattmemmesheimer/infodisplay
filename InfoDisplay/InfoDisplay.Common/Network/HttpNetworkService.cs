b94e10558e3a4555e3b629d52e9aaba9d14ec5ef
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-12 01:52:19 -0500

    Commenting.

--- InfoDisplay/InfoDisplay.Common/Network/HttpNetworkService.cs
+++ InfoDisplay/InfoDisplay.Common/Network/HttpNetworkService.cs
@@ -3,9 +3,12 @@ using System.Threading.Tasks;
 
 namespace InfoDisplay.Common.Network
 {
-
+    /// <summary>
+    /// HTTP implementation of <see cref="INetworkService"/>.
+    /// </summary>
     public class HttpNetworkService : INetworkService
     {
+        /// <see cref="INetworkService.GetStringAsync"/>
         public async Task<string> GetStringAsync(string url)
         {
             var client = new WebClient();
--- InfoDisplay/InfoDisplay.Common/Network/INetworkService.cs
+++ InfoDisplay/InfoDisplay.Common/Network/INetworkService.cs
@@ -2,10 +2,16 @@
 
 namespace InfoDisplay.Common.Network
 {
-
+    /// <summary>
+    /// Network service interface.
+    /// </summary>
     public interface INetworkService
     {
+        /// <summary>
+        /// Asynchronously downloads a string from the specified URL.
+        /// </summary>
+        /// <param name="url">URL to download from.</param>
+        /// <returns>Downloaded string.</returns>
         Task<string> GetStringAsync(string url);
     }
-
 }
