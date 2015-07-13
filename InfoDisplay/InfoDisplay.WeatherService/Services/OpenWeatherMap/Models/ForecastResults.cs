f61ec7ce02033636cef2616868d554cd9fd5dc8b
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-12 21:32:45 -0500

    Starting working on weather forecast view.

--- InfoDisplay/InfoDisplay.WeatherService/Services/OpenWeatherMap/Models/ForecastResults.cs
+++ InfoDisplay/InfoDisplay.WeatherService/Services/OpenWeatherMap/Models/ForecastResults.cs
@@ -1,5 +1,6 @@
 ﻿using InfoDisplay.Common.Util.Json;
 using InfoDisplay.WeatherService.Models;
+using Microsoft.Practices.Prism.Mvvm;
 using Newtonsoft.Json;
 
 namespace InfoDisplay.WeatherService.Services.OpenWeatherMap.Models
@@ -7,14 +8,24 @@ namespace InfoDisplay.WeatherService.Services.OpenWeatherMap.Models
     /// <summary>
     /// Concrete implementation of <see cref="IForecastResults"/> for Open Weather Map service.
     /// </summary>
-    public class ForecastResults : IForecastResults
+    public class ForecastResults : BindableBase, IForecastResults
     {
         #region Properties
 
         /// <see cref="IForecastResults.Days"/>
         [JsonProperty(PropertyName = "list")]
-        [JsonConverter(typeof(ConcreteTypeConverter<ForecastResult[]>))]
-        public IForecastResult[] Days { get; set; }
+        [JsonConverter(typeof (ConcreteTypeConverter<ForecastResult[]>))]
+        public IForecastResult[] Days
+        {
+            get { return _days; }
+            set { SetProperty(ref _days, value); }
+        }
+
+        #endregion
+
+        #region Fields
+
+        private IForecastResult[] _days;
 
         #endregion
     }
