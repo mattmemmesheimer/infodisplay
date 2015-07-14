fbcdfc068e74a13d16d2541186f90c2c60529331
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-13 21:46:36 -0500

    Committing some test code for getting total CPU usage.

--- InfoDisplay/InfoDisplay.Gui/ViewModels/ISystemStatsViewModel.cs
+++ InfoDisplay/InfoDisplay.Gui/ViewModels/ISystemStatsViewModel.cs
@@ -1,10 +1,15 @@
-﻿namespace InfoDisplay.Gui.ViewModels
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace InfoDisplay.Gui.ViewModels
 {
     /// <summary>
     /// Defines an interface for the system stats view model.
     /// </summary>
     public interface ISystemStatsViewModel
     {
-         
+        Task GetSystemStatsAsync();
+
+        ICommand RefreshCommand { get; }
     }
 }
--- InfoDisplay/InfoDisplay.Gui/ViewModels/SystemStatsViewModel.cs
+++ InfoDisplay/InfoDisplay.Gui/ViewModels/SystemStatsViewModel.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Practices.Prism.Mvvm;
+﻿using System;
+using System.Linq;
+using System.Management;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using InfoDisplay.Gui.Command;
+using Microsoft.Practices.Prism.Mvvm;
 
 namespace InfoDisplay.Gui.ViewModels
 {
@@ -7,7 +13,53 @@ namespace InfoDisplay.Gui.ViewModels
     /// </summary>
     public class SystemStatsViewModel : BindableBase, ISystemStatsViewModel
     {
-         
+        #region Properties
+
+        public string TotalCpuUsage
+        {
+            get { return _totalCpuUsage; }
+            set { SetProperty(ref _totalCpuUsage, value); }
+        }
+
+        public ICommand RefreshCommand { get; private set; }
+
+        #endregion
+
+        public SystemStatsViewModel()
+        {
+            RefreshCommand = new AwaitableDelegateCommand(GetSystemStatsAsync);
+        }
+
+        public async Task GetSystemStatsAsync()
+        {
+            TotalCpuUsage = "Loading...";
+            TotalCpuUsage = await Task.Factory.StartNew(() => GetTotalCpuUsage());
+        }
+
+        private string GetTotalCpuUsage()
+        {
+            var searcher = new ManagementObjectSearcher("select * from Win32_PerfFormattedData_PerfOS_Processor");
+            var cpuTimes = searcher.Get()
+                .Cast<ManagementObject>()
+                .Select(mo => new
+                {
+                    Name = mo["Name"],
+                    Usage = mo["PercentProcessorTime"]
+                }
+                )
+                .ToList();
+            //The '_Total' value represents the average usage across all cores,
+            //and is the best representation of overall CPU usage
+            var query = cpuTimes.Where(x => x.Name.ToString() == "_Total").Select(x => x.Usage);
+            var cpuUsage = query.SingleOrDefault();
+            return cpuUsage.ToString();
+        }
+
+        #region Fields
+
+        private string _totalCpuUsage;
+
+        #endregion
     }
 
 }
