f85c88b7e11ea82c5d6807319ee42a9219fde46d
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-16 22:05:56 -0500

    Added a fullscreen mode that can be toggled with F11 key.

--- InfoDisplay/InfoDisplay.Shell/PrismAppShell.xaml.cs
+++ InfoDisplay/InfoDisplay.Shell/PrismAppShell.xaml.cs
@@ -1,27 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows.Input;
 
 namespace InfoDisplay.Shell
 {
     /// <summary>
     /// Interaction logic for PrismAppShell.xaml
     /// </summary>
-    public partial class PrismAppShell : Window
+    public partial class PrismAppShell
     {
         public PrismAppShell()
         {
+            _viewModel = new PrismAppShellViewModel();
+            DataContext = _viewModel;
             InitializeComponent();
         }
+
+        private void Window_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F11)
+            {
+                _viewModel.Fullscreen = !_viewModel.Fullscreen;
+            }
+            if (e.Key == Key.Escape && _viewModel.Fullscreen)
+            {
+                _viewModel.Fullscreen = false;
+            }
+        }
+
+        private readonly PrismAppShellViewModel _viewModel;
     }
 }
---
+++ InfoDisplay/InfoDisplay.Shell/PrismAppShellViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using Microsoft.Practices.Prism.Mvvm;
+
+namespace InfoDisplay.Shell
+{
+    public class PrismAppShellViewModel : BindableBase
+    {
+        #region Properties
+
+        public bool Fullscreen
+        {
+            get { return _fullscreen; }
+            set
+            {
+                if (value)
+                {
+                    WindowState = WindowState.Maximized;
+                    WindowStyle = WindowStyle.None;
+                }
+                else
+                {
+                    WindowState = WindowState.Normal;
+                    WindowStyle = WindowStyle.SingleBorderWindow;
+                }
+                SetProperty(ref _fullscreen, value);
+            }
+        }
+
+        public WindowStyle WindowStyle
+        {
+            get { return _windowStyle; }
+            private set { SetProperty(ref _windowStyle, value); }
+        }
+
+        public WindowState WindowState
+        {
+            get { return _windowState; }
+            private set { SetProperty(ref _windowState, value); }
+        }
+
+        #endregion
+
+        public PrismAppShellViewModel()
+        {
+            Fullscreen = false;
+        }
+
+        #region Fields
+
+        private WindowStyle _windowStyle;
+        private WindowState _windowState;
+        private bool _fullscreen;
+
+        #endregion
+    }
+}
